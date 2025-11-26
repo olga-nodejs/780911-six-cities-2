820f32ae1f59be6733e583c5433f16d7a11346c6
Olga <42041051+OlgaS-codes@users.noreply.github.com>
2025-11-26 23:10:49 +0200

    feat: add Offer DI container

---
+++ src/shared/modules/offer/offer.container.ts
@@ -0,0 +1,20 @@
+import { Container } from 'inversify';
+import { OfferService } from './offer-service.interface.js';
+import { Component } from '../../types/component.enum.js';
+import { DefaultOfferService } from './default-offer.service.js';
+import { types } from '@typegoose/typegoose';
+import { OfferEntity, OfferModel } from './offer.entity.js';
+
+export function createOfferContainer() {
+  const offerContainer = new Container();
+  offerContainer
+    .bind<OfferService>(Component.OfferService)
+    .to(DefaultOfferService)
+    .inSingletonScope();
+
+  offerContainer
+    .bind<types.ModelType<OfferEntity>>(Component.OfferModel)
+    .toConstantValue(OfferModel);
+
+  return offerContainer;
+}
