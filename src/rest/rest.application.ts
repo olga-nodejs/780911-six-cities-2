eb069a711712ec94c661221c66c0ed34d8e4192d
Olga <42041051+OlgaS-codes@users.noreply.github.com>
2025-12-09 12:57:44 +0200

    feat: add init server on rest

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -1,3 +1,4 @@
+import express, { Express } from 'express';
 import { RestApplicationInterface } from './rest.interface.js';
 import { type Logger } from '../shared/libs/Logger/index.js';
 import { inject, injectable } from 'inversify';
@@ -9,11 +10,14 @@ import { DBClient } from '../shared/libs/db-client/db-client.interface.js';
 
 @injectable()
 export class RestApplication implements RestApplicationInterface {
+  private readonly server: Express;
   constructor(
     @inject(Component.Logger) private readonly logger: Logger,
     @inject(Component.Config) private readonly config: Config<RestSchema>,
     @inject(Component.DBClient) private readonly db: DBClient
-  ) {}
+  ) {
+    this.server = express();
+  }
 
   private async initDB() {
     const mongoUri = getMongoURI(
@@ -27,11 +31,21 @@ export class RestApplication implements RestApplicationInterface {
     return this.db.connect(mongoUri);
   }
 
+  private async initServer() {
+    const port = this.config.get('PORT');
+    this.server.listen(port);
+    console.log({ port });
+  }
+
   async init() {
     this.logger.info(`PORT: ${this.config.get('PORT')}`);
     this.logger.info('Application initialization started');
     this.logger.info('DB initialization started');
     await this.initDB();
     this.logger.info('DB initialization completed');
+    await this.initServer();
+    this.logger.info(
+      `🚀 Server started on http://localhost:${this.config.get('PORT')}`
+    );
   }
 }
