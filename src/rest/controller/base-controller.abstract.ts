2753f23f1fd5551593f4d6ec0b984212bb0b3bef
Olga <42041051+OlgaS-codes@users.noreply.github.com>
2025-12-09 14:30:55 +0200

    feat: add base controller and controller interface

---
+++ src/rest/controller/base-controller.abstract.ts
@@ -0,0 +1,43 @@
+import { injectable } from 'inversify';
+import { Response, Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { Logger } from '../../shared/libs/Logger/index.js';
+import { Controller } from './controller.interface.js';
+import { Route } from '../../shared/types/route.interface.js';
+
+const DEFAULT_CONTENT_TYPE = 'application/json';
+
+@injectable()
+export abstract class BaseController implements Controller {
+  private readonly _router;
+  constructor(protected readonly logger: Logger) {
+    this._router = Router();
+  }
+
+  get router() {
+    return this._router;
+  }
+
+  public addRoute(route: Route) {
+    this._router[route.method](route.path, route.handler.bind(this));
+    this.logger.info(
+      `Route registered: ${route.method.toUpperCase()} ${route.path}`
+    );
+  }
+
+  public send<T>(res: Response, statusCode: number, data: T): void {
+    res.type(DEFAULT_CONTENT_TYPE).status(statusCode).json(data);
+  }
+
+  public created<T>(res: Response, data: T): void {
+    this.send(res, StatusCodes.CREATED, data);
+  }
+
+  public noContent<T>(res: Response, data: T): void {
+    this.send(res, StatusCodes.NO_CONTENT, data);
+  }
+
+  public ok<T>(res: Response, data: T): void {
+    this.send(res, StatusCodes.OK, data);
+  }
+}
--- src/rest/index.ts
+++ src/rest/index.ts
@@ -1,3 +1,5 @@
 export * from './rest.application.js';
 export * from './rest.interface.js';
 export * from './rest.container.js';
+export * from './controller/base-controller.abstract.js';
+export * from './controller/controller.interface.js';
